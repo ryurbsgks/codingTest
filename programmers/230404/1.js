cf4d452392b5ecc676d8abf96f70d2d9fdf05d4c
ryurbsgks <ryurbsgks5114@gmail.com>
2023-04-04 21:25:20 +0900

    programmers 230404 finish

---
+++ programmers/230404/1.js
@@ -0,0 +1,14 @@
+// 두 수의 합
+
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+function solution(num1, num2) {
+  var answer = -1;
+
+  answer = num1 + num2;
+
+  return answer;
+}
+
+console.log(solution(2, 3)); // 5
+console.log(solution(100, 2)); // 102
---
+++ programmers/230404/3.js
@@ -0,0 +1,16 @@
+// 중복된 숫자 개수
+
+// 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+
+function solution(array, n) {
+  var answer = 0;
+
+  let newArr = array.filter( (el) => el === n);
+  
+  answer = newArr.length;
+
+  return answer;
+}
+
+console.log(solution([1, 1, 2, 3, 4, 5], 1)); // 2
+console.log(solution([0, 2, 3, 4], 1)); // 0
