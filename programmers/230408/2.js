3458142422d1a90a68c1c120ffe17779a06ae929
ryurbsgks <ryurbsgks5114@gmail.com>
2023-04-08 18:50:18 +0900

    programmers 230408 finish

---
+++ programmers/230408/2.js
@@ -0,0 +1,15 @@
+// 배열의 평균값
+
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+  var answer = 0;
+
+  let sum = numbers.reduce( (acc, cur) => acc + cur, 0);
+  answer = sum / numbers.length;
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 5.5
+console.log(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99])) // 94
---
+++ programmers/230408/3.js
@@ -0,0 +1,23 @@
+// 옷가게 할인 받기
+
+// 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+// 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(price) {
+  var answer = 0;
+
+  if (price >= 500000) {
+    answer = Math.floor(price * 0.8);
+  } else if (price >= 300000) {
+    answer = Math.floor(price * 0.9);
+  } else if (price >= 100000) {
+    answer = Math.floor(price * 0.95);
+  } else {
+    answer = price;
+  }
+
+  return answer;
+}
+
+console.log(solution(150000)) // 142500
+console.log(solution(580000)) // 464000
