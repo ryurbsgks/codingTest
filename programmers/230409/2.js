c8fff2012a1db50a438562098e3f80a1af125b6e
ryurbsgks <ryurbsgks5114@gmail.com>
2023-04-09 17:23:32 +0900

    programmers 230409 finish

---
+++ programmers/230409/2.js
@@ -0,0 +1,15 @@
+// 배열 뒤집기
+
+// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+  var answer = [];
+
+  num_list.map( (el) => answer.unshift(el));
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4, 5])) // [5, 4, 3, 2, 1]
+console.log(solution([1, 1, 1, 1, 1, 2])) // [2, 1, 1, 1, 1, 1]
+console.log(solution([1, 0, 1, 1, 1, 3, 5])) // [5, 3, 1, 1, 1, 0, 1]
