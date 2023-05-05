92a49b9e96cd5bbd9696ed9a4af5db98ab3680b6
ryurbsgks <ryurbsgks5114@gmail.com>
2023-05-05 20:56:47 +0900

    programmers 230505 finish

---
+++ programmers/230505/1.js
@@ -0,0 +1,17 @@
+// 뒤에서 5등까지
+
+// 정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+  var answer = [];
+
+  num_list.sort( (a, b) => a - b);
+  
+  for (let i = 0; i < 5; i++) {
+    answer.push(num_list[i]);
+  }
+
+  return answer;
+}
+
+console.log(solution([12, 4, 15, 46, 38, 1, 14])) // [1, 4, 12, 14, 15]
---
+++ programmers/230505/3.js
@@ -0,0 +1,28 @@
+// 문자열 묶기
+
+// 문자열 배열 strArr이 주어집니다. strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성해 주세요.
+
+function solution(strArr) {
+  var answer = 0;
+
+  let obj = {};
+
+  strArr.map( (el) => {
+    if (obj[el.length]) {
+      return obj[el.length] = obj[el.length] + 1;
+    }
+
+    obj[el.length] = 1;
+  });
+
+  for (const key in obj) {
+    if (answer < obj[key]) {
+      answer = obj[key];
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(["a","bc","d","efg","hi"])) // 2
+console.log(solution(["a","bc","d","efg","hi", "easdf", "dddd", "dddd", "1111"])) // 2
