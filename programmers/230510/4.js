5b0f15a8189b0ac632346c13dece22fbc8129123
ryurbsgks <ryurbsgks5114@gmail.com>
2023-05-10 20:36:29 +0900

    programmers 230510 finish

---
+++ programmers/230510/4.js
@@ -0,0 +1,14 @@
+// 소문자로 바꾸기
+
+// 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+function solution(myString) {
+  var answer = '';
+
+  answer = myString.toLowerCase();
+
+  return answer;
+}
+
+console.log(solution("aBcDeFg")) // "abcdefg"
+console.log(solution("aaa")) // "aaa"
