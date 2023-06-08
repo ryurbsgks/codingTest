4edac64ad6d3a1610a5d68187e48d6d3c07456d4
ryurbsgks <ryurbsgks5114@gmail.com>
2023-06-08 13:47:51 +0900

    programmers 230608 finish

---
+++ programmers/230608/3.js
@@ -0,0 +1,33 @@
+// 체육복
+
+// 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+// 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+
+function solution(n, lost, reserve) {
+  var answer = 0;
+
+  let newLost = lost.filter( (el) => !reserve.includes(el)).sort( (a, b) => a - b);
+  let newReserve = reserve.filter( (el) => !lost.includes(el)).sort( (a, b) => a - b);
+
+  answer = n - newLost.length;
+
+  newLost.map( (el) => {
+    for (let i = 0; i < newReserve.length; i++) {
+      if (el - 1 === newReserve[i]) {
+        answer++;
+        newReserve.splice(i, 1);
+        break;
+      } else if (el + 1 === newReserve[i]) {
+        answer++;
+        newReserve.splice(i, 1);
+        break;
+      }
+    }
+  });
+
+  return answer;
+}
+
+console.log(solution(5, [2, 4], [1, 3, 5])) // 5
+console.log(solution(5, [2, 4], [3])) // 4
+console.log(solution(3, [3], [1])) // 2
