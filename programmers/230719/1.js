bbb76625990dff97410a163bc453a8bc44ea5c87
ryurbsgks <ryurbsgks5114@gmail.com>
2023-07-19 20:22:12 +0900

    programmers 230719 finish

---
+++ programmers/230719/1.js
@@ -0,0 +1,67 @@
+// 전력망을 둘로 나누기
+
+// n개의 송전탑이 전선을 통해 하나의 트리 형태로 연결되어 있습니다. 당신은 이 전선들 중 하나를 끊어서 현재의 전력망 네트워크를 2개로 분할하려고 합니다. 이때, 두 전력망이 갖게 되는 송전탑의 개수를 최대한 비슷하게 맞추고자 합니다.
+// 송전탑의 개수 n, 그리고 전선 정보 wires가 매개변수로 주어집니다. 전선들 중 하나를 끊어서 송전탑 개수가 가능한 비슷하도록 두 전력망으로 나누었을 때, 두 전력망이 가지고 있는 송전탑 개수의 차이(절대값)를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(n, wires) {
+  var answer = -1;
+
+  let obj = {};
+
+  wires.map( (el) => {
+
+    let [a, b] = el;
+
+    if (!obj[a]) {
+      obj[a] = [];
+    }
+
+    if (!obj[b]) {
+      obj[b] = [];
+    }
+
+    obj[a].push(b);
+    obj[b].push(a);
+  });
+
+  const tree = (root, exception) => {
+
+    let count = 0;
+    let queue = [root];
+    let arr = [];
+
+    arr[root] = tree;
+
+    while (queue.length) {
+
+      let cur = queue.pop();
+
+      obj[cur].map( (el) => {
+        if (el !== exception && !arr[el]) {
+          arr[el] = true;
+          queue.push(el);
+        }
+      });
+
+      count++;
+    }
+
+    return count;
+  };
+
+  answer = 100;
+
+  wires.map( (el) => {
+
+    let [a, b] = el;
+    let diff = Math.abs(tree(a, b) - tree(b, a));
+
+    answer = answer > diff ? diff : answer;
+  });
+
+  return answer;
+}
+
+console.log(solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]])) // 3
+console.log(solution(4, [[1,2],[2,3],[3,4]])) // 0
+console.log(solution(7, [[1,2],[2,7],[3,7],[3,4],[4,5],[6,7]])) // 1
