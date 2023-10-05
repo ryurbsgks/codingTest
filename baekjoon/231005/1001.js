927724f91149e5e3a829be53440e913835544565
ryurbsgks <ryurbsgks5114@gmail.com>
2023-10-05 22:24:47 +0900

    baekjoon 231005 finish

---
+++ baekjoon/231005/1001.js
@@ -0,0 +1,10 @@
+// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+const fs = require("fs");
+const input = fs.readFileSync("/home/ryurbsgks5114/codingTest/baekjoon/input.txt").toString().split(" ");
+
+function solution(A, B) {
+  return Number(A) - Number(B);
+}
+
+console.log(solution(input[0], input[1])); // 1
---
+++ baekjoon/231005/2557.js
@@ -0,0 +1,7 @@
+// Hello World!를 출력하시오.
+
+function solution() {
+  return "Hello World!";
+}
+
+console.log(solution()); // Hello World!
