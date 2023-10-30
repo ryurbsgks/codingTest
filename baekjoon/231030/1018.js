3d4266351fb25b5b9f65540d7a4bf99fdbad5b61
ryurbsgks <ryurbsgks5114@gmail.com>
2023-10-30 17:12:38 +0900

    baekjoon 231030 finish

---
+++ baekjoon/231030/1018.js
@@ -0,0 +1,41 @@
+// 지민이는 자신의 저택에서 MN개의 단위 정사각형으로 나누어져 있는 M×N 크기의 보드를 찾았다. 어떤 정사각형은 검은색으로 칠해져 있고, 나머지는 흰색으로 칠해져 있다. 지민이는 이 보드를 잘라서 8×8 크기의 체스판으로 만들려고 한다.
+// 체스판은 검은색과 흰색이 번갈아서 칠해져 있어야 한다. 구체적으로, 각 칸이 검은색과 흰색 중 하나로 색칠되어 있고, 변을 공유하는 두 개의 사각형은 다른 색으로 칠해져 있어야 한다. 따라서 이 정의를 따르면 체스판을 색칠하는 경우는 두 가지뿐이다. 하나는 맨 왼쪽 위 칸이 흰색인 경우, 하나는 검은색인 경우이다.
+// 보드가 체스판처럼 칠해져 있다는 보장이 없어서, 지민이는 8×8 크기의 체스판으로 잘라낸 후에 몇 개의 정사각형을 다시 칠해야겠다고 생각했다. 당연히 8*8 크기는 아무데서나 골라도 된다. 지민이가 다시 칠해야 하는 정사각형의 최소 개수를 구하는 프로그램을 작성하시오.
+
+const fs = require("fs");
+const input = fs.readFileSync("/home/ryurbsgks5114/codingTest/baekjoon/input.txt").toString().trim().split("\n");
+
+function solution(arr) {
+
+  let N = Number(arr[0].split(" ")[0]);
+  let M = Number(arr[0].split(" ")[1]);
+  let newArr = arr.slice(1, arr.length);
+  let line = ["WBWBWBWB", "BWBWBWBW"];
+  let min = 64;
+
+  for (let i = 0; i <= N - 8; i++) {
+    for (let j = 0; j <= M - 8; j++) {
+      for (let k = 0; k < 2; k++) {
+
+        let count = 0;
+
+        for (let l = i; l < i + 8; l++) {
+          for (let m = j; m < j + 8; m++) {
+            if (newArr[l][m] !== line[(l + k) % 2][m - j]) {
+              count++;
+            }
+          }
+        }
+
+        if (min > count) {
+          min = count;
+        }
+
+      }
+    }
+  }
+
+  return min;
+}
+
+console.log(solution(input));
