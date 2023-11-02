e9ce14bbc3d73733319764e2f2e53626304bddb1
ryurbsgks <ryurbsgks5114@gmail.com>
2023-11-03 03:44:05 +0900

    baekjoon 231103 finish

---
+++ baekjoon/231103/11651.js
@@ -0,0 +1,28 @@
+// 2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+const fs = require("fs");
+const input = fs.readFileSync("/home/ryurbsgks5114/codingTest/baekjoon/input.txt").toString().trim().split("\n");
+const newInput = input.slice(1, input.length).map( (el) => el.split(" ").map( (element) => Number(element)));
+
+function solution(arr) {
+
+  let answer = "";
+  
+  arr.sort( (a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    } else {
+      return a[1] - b[1];
+    }
+  });
+
+  arr.map( (el) => {
+    answer = `${answer}${el[0]} ${el[1]}\n`;
+  });
+
+  answer = answer.slice(0, answer.length - 1);
+
+  return answer;
+}
+
+console.log(solution(newInput));
